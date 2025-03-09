40119dd64d7ffc38042d199352d4a1dbef2bfa63
shadybraden <shadybraden@shady.icu>
2025-03-09 18:04:13 -0400

    fix script

--- docker/proxy/restart_proxy.sh
+++ docker/proxy/restart_proxy.sh
@@ -4,8 +4,5 @@ docker compose pull
 docker compose stop gluetun socks5
 docker compose --env-file ../.env -v up -d
 sleep 10
-COUNTRY=$(curl --socks5 localhost:1080 ifconfig.co/json)
-curl \
-  -H "Priority: min" \
-  -d "proxy ip now in: $COUNTRY" \
-  https://ntfy.example.com/topic
+IP_DATA=$(curl --socks5 localhost:1080 ifconfig.co/json)
+curl -H "Priority: min" -d "proxy ip now in: $IP_DATA" https://ntfy.example.com/topic
